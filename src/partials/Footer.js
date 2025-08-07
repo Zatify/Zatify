9768c6979d20f6b152854de33cc636f8dd55f1bb
hoangohuc1405 <phungbuuhoangphuc828@gmail.com>
2025-08-07 00:24:46 -0800

    footer

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -13,25 +13,22 @@ const Footer = () => {
       )}
       {/* Top Section */}
       {!isContactPage && (
-        <div className="flex flex-col items-center md:flex-row justify-between gap-20 2xl:gap-28 px-4 py-14 2xl:px-0 max-w-7xl h-auto 0.5xl:h-[110vh] 2xl:h-[120vh] mx-auto relative z-10">
+        <div className="flex flex-col items-center md:flex-row justify-between gap-20 2xl:gap-28 px-4 py-14 2xl:px-0 max-w-7xl h-auto 0.5xl:h-[100vh] 2xl:h-[110vh] mx-auto relative z-10">
           {/* Left: Contact Info */}
           <div className="min-w-[320px] max-w-[650px] 2xl:max-w-[700px]">
             <h2 className="text-4xl md:text-6xl 2xl:text-7xl leading-tight mb-4 font-roboto">
-              We are always ready to help you and answer your questions
+              Chúng tôi luôn sẵn sàng hỗ trợ và giải đáp mọi thắc mắc của bạn!
             </h2>
-            <p className="text-[16px] 2xl:text-[20px] mt-10 text-white mb-8 max-w-md font-manrope">
-              Pacific hake false trevally queen parrotfish black prickleback mosshead warbonnet sweeper! Greenling sleeper.
-            </p>
             <div className="grid grid-cols-2 gap-y-6 gap-x-10 text-sm max-w-lg mt-16">
               <div>
-                <h3 className="font-manrope font-semibold text-[17px] 2xl:text-[21px] mb-2">Call Center</h3>
+                <h3 className="font-manrope font-semibold text-[17px] 2xl:text-[21px] mb-2">Hotline</h3>
                 <div className="text-[15px] 2xl:text-[18px] leading-normal mt-6 font-manrope mb-4">
                   <p>0389 603 339</p>
                   <p>0919 676 808</p>
                 </div>
               </div>
               <div>
-                <h3 className="font-semibold font-manrope mb-2 text-[17px] 2xl:text-[21px]">Our Location</h3>
+                <h3 className="font-semibold font-manrope mb-2 text-[17px] 2xl:text-[21px]">Địa chỉ</h3>
                 <div className="text-[15px] 2xl:text-[18px] leading-normal mt-6 font-manrope mb-4">
                   <p>1015/32 đường Huỳnh Tấn Phát, phường Phú Thuận Quận 7, TP.HCM</p>
                 </div>
@@ -51,7 +48,7 @@ const Footer = () => {
               <form className="space-y-4">
                 <input
                   type="text"
-                  placeholder="Full name"
+                  placeholder="Họ tên"
                   className="w-full border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 2xl:px-6 lg:py-4 2xl:text-lg"
                 />
                 <input
@@ -61,17 +58,17 @@ const Footer = () => {
                 />
                 <input
                   type="text"
-                  placeholder="Subject"
+                  placeholder="Tiêu đề"
                   className="w-full border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 2xl:px-6 lg:py-4 2xl:text-lg"
                 />
                 <textarea
-                  placeholder="Message"
+                  placeholder="Nội dung"
                   rows="4"
                   className="w-full border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-cyan-400 2xl:px-6 lg:py-4 2xl:text-lg"
                 />
                 <div className="gradient-border rounded-md inline-block p-[1px]">
                   <button className="justify-center text-sm text-black bg-white rounded-[0.65rem] px-4 py-2 hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500 hover:text-white transition flex items-center gap-1 w-[160px] h-[50px]">
-                    Send Message
+                    Gửi mail ngay
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-4 w-4"
@@ -90,24 +87,9 @@ const Footer = () => {
         </div>
       )}
       {/* Middle Section: Slogan */}
-      <div className="max-w-7xl border-b border-gray-300 h-[30vh] mx-4 0.5xl:mx-auto flex flex-col 0.5xl:flex-row items-start 0.5xl:items-center justify-around 0.5xl:justify-between relative z-10">
-        <div className="text-4xl md:text-7xl mb-2 bg-gradient-to-r from-cyan-300  to-blue-500 text-transparent bg-clip-text select-none font-roboto">
-          It’s blow your mind!<br />Meet Neural Networks
-        </div>
-        <div className="gradient-border rounded-md inline-block p-[1px]">
-          <button className="justify-center text-sm text-white bg-[#18191b] rounded-[0.65rem] px-4 py-2 hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500 hover:text-whitezzzzzzzzz transition flex items-center gap-1 w-[160px] h-[50px]">
-            Get a Quote
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-            </svg>
-          </button>
+      <div className="max-w-7xl border-b border-gray-300 h-[20vh] md:h-[30vh] mx-4 0.5xl:mx-auto flex flex-col 0.5xl:flex-row items-start 0.5xl:items-center justify-around 0.5xl:justify-between relative z-10">
+        <div className="text-4xl pb-2 md:text-7xl mb-2 bg-gradient-to-r from-cyan-300  to-blue-500 text-transparent bg-clip-text select-none font-roboto">
+          Zatify - ZNS thần tốc nâng tầm doanh nghiệp!
         </div>
       </div>
       {/* Bottom Section */}
@@ -120,7 +102,7 @@ const Footer = () => {
             className="text-[15vw] sm:text-[15vw] md:text-[8vw] lg:text-[9vw] 0.5xl:text-[7vw] font-bold text-transparent bg-clip-text mb-6 select-none leading-none whitespace-nowrap"
             style={{ WebkitTextStroke: '2px #5f5fff', color: 'transparent' }}
           >
-            since 2024
+            since 2023
           </div>
           <p className="text-xs text-gray-300 mb-4"><span className="text-cyan-200">©<a href="#" className="underline">Aiero</a> 2025.</span> All rights reserved.</p>
         </div>
